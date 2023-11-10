27c3dfd566f5e3c8734299489a91e26c9b96fdda
Rock Clapps <rockclapps@gmail.com>
2023-11-10 13:23:32 -0500

    First commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+vim.opt.langmap = "hjklHJKLneioNEIO;neioNEIOhjklHJKL"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.cmd('colorscheme desert')
+
+vim.api.nvim_create_user_command('Startlsp', function()
+	require'lspconfig'.pyright.setup{}
+	require'lspconfig'.gopls.setup{}
+	require'lspconfig'.clangd.setup{}
+	require'lspconfig'.lua_ls.setup{}
+	require'lspconfig'.tsserver.setup{}
+	vim.cmd('edit!')
+end, {})
